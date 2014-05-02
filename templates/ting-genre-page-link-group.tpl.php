57733a98d659166b858d520be0ee77b2f044aa73
Arni <al@randersbib.dk>
2014-05-02 12:09:27 +0200

    added title

--- templates/ting-genre-page-link-group.tpl.php
+++ templates/ting-genre-page-link-group.tpl.php
@@ -13,23 +13,25 @@
 ?>
 
 <?php if ($rows): ?>
-  <div class="related-links-header">
-    <div class="related-links-title">
-      <h1>Genveje</h1>
+  <div class="genre-page-related-links">
+    <div class="related-links-header">
+      <div class="related-links-title">
+        <h1>Genveje</h1>
+      </div>
+    </div>
+    <div class="ting-genre-page-link-group genre-list-big group-blocks--wrapper <?php print $classes ?>" <?php print $attributes ?>>
+      <?php foreach ($rows as $row): ?>
+        <div class="<?php print $row['class']; ?> group-blocks--inner">
+          <?php foreach ($row['columns'] as $column_class => $column): ?>
+            <div class="<?php print $column_class; ?>">
+              <a href="<?php print $column['url']; ?>">
+                <h2><?php print render($column['title']); ?></h2>
+                <?php print render($column['image']); ?>							
+              </a>
+            </div>
+          <?php endforeach; ?>
+        </div>
+      <?php endforeach; ?>
     </div>
   </div>
-	<div class="ting-genre-page-link-group genre-list-big group-blocks--wrapper <?php print $classes ?>" <?php print $attributes ?>>
-		<?php foreach ($rows as $row): ?>
-			<div class="<?php print $row['class']; ?> group-blocks--inner">
-				<?php foreach($row['columns'] as $column_class => $column): ?>
-					<div class="<?php print $column_class; ?>">
-						<a href="<?php print $column['url']; ?>">
-              <h2><?php print render($column['title']); ?></h2>
-							<?php print render($column['image']); ?>							
-						</a>
-					</div>
-				<?php endforeach; ?>
-			</div>
-		<?php endforeach; ?>
-	</div>
 <?php endif; ?>
